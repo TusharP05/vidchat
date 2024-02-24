be06dc06393ea4a56f74f72b6dc2a469f87b02e4
Rahul <rahulphulbhati@gmail.com>
2024-02-24 12:45:19 +0530

    zego service renewed

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -5,11 +5,13 @@ import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 const RoomPage = () =>{
     const {roomId} = useParams();
     const myMeeting= async(element) =>{
-       const appID= 1482766618;
-       const serverSecret= "a974719b2ab8ccacdb3781b5273fe805";
+       const appID= 815651400;
+    //    1482766618;
+       const serverSecret= "4e273a2cd103faddda17374d67a12400";
+    //    "a974719b2ab8ccacdb3781b5273fe805";
        const kitToken= ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Tushar");
        const zc=ZegoUIKitPrebuilt.create(kitToken);
-       zc.joinRoom({
+       await zc.joinRoom({
         container:element,
         scenario: {
             mode: ZegoUIKitPrebuilt.OneONoneCall,
